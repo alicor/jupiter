31722fa5fd1491f8670ddd3972e4eb1a60554260
Alice Corradini <alice.corradini.wr@gmail.com>
2015-12-18 17:18:47 +0100

    First commit

---
+++ js/index.js
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    show_alert: function(message, title){
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+        if (!navigator.camera)
+            this.show_alert("Not found", "Camera");
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        window.addEventListener("batterystatus", this.onBatteryStatus, false);
+        window.addEventListener("batterycritical", this.onBatteryCritical, false);
+        window.addEventListener("batterylow", this.onBatteryLow, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    onBatteryStatus: function(info){
+        var level = document.getElementById('.level');
+        var isPlugged = document.getElementById('.is_plugged');
+
+        level.innerHTML=info.level;
+        isPlugged.innerHTML=info.isPlugged();
+    },
+    onBatteryCritical: function(info) {
+        console.log("Battery Level Critical " + info.level + "%\nRecharge Soon!");
+    },
+    onBatteryLow: function(info) {
+        // Handle the battery low event
+        console.log("Battery Level Low " + info.level + "%");
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
